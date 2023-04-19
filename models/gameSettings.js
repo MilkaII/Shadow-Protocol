01204d61f9dfdfc12fa4c896ab8682aa68942de5
MilkaII <fabiogaspar2603@gmail.com>
2023-04-19 13:18:39 +0100

    Place card on the board and attack opponent card

--- models/gameSettings.js
+++ models/gameSettings.js
@@ -4,3 +4,5 @@
 module.exports.nCards = 3;
 module.exports.nCardsPerTurn = 1;
 module.exports.MaxCards = 6;
+module.exports.nChipsPerTurn = 3;
+module.exports.MaxChips = 10;
--- public/javascripts/ui/actions.js
+++ public/javascripts/ui/actions.js
@@ -36,9 +36,19 @@ async function getDecksInfo() {
         400,
         500,
         null,
-        GameInfo.images.card
+        GameInfo.images.card,
+        dragndrop
       );
+
+    if (GameInfo.oppDeck) GameInfo.oppDeck.update(GameInfo.matchdeck);
+    else
+      GameInfo.oppDeck = new Deck(GameInfo.matchdeck, null, null, null, null);
   }
+}
+async function dragndrop(x,y,card) {
+  let pos = GameInfo.board.getPlayerColumnAt(x,y);
+  playCard(card, pos);
+  //alert(pos);
 }
 
 async function endturnAction() {
@@ -66,6 +76,68 @@ async function ChooseDeck2Action() {
       GameInfo.prepareUI();
     } else alert("Something went wrong choosing the deck.");
   }
+}
+
+async function getBoardInfo() {
+  let result = await requestBoardInfo();
+  if (!result.successful) {
+    alert("Something is wrong with the game please login again!");
+    window.location.pathname = "index.html";
+  } else {
+    GameInfo.gameboard = result.board;
+    if (GameInfo.board) GameInfo.board.update(GameInfo.gameboard);
+    else
+      GameInfo.board = new Board(
+        GameInfo.gameboard,
+        GameInfo.game.player.name,
+        GameInfo.game.opponents[0].name,
+        400,
+        150,
+        650,
+        380,
+        30,
+        GameInfo.images.boardbg,
+        GameInfo.images.card,
+        clickActionAttack
+      );
+  }
+}
+
+
+// remake this
+let selectedCards = [];
+async function clickActionAttack(x,y) {
+  let card = GameInfo.board.getCardAt(x, y);
+  selectedCards.push(card);
+
+  if (selectedCards.length === 2) {
+    await attackCard(selectedCards[0], selectedCards[1]);
+    selectedCards = [];
+  }
+}
+
+async function playCard(card, position) {
+  //if (confirm(`Do you want to play the "${card.ugc_crd_name}" card?`)) {
+    let result = await requestPlayCard(card.ugc_id, position);
+    if (result.successful) {
+      await getGameInfo();
+      await getBoardInfo();
+      await getDecksInfo();
+      alert("Card Played!");
+    }
+  //}
+}
+
+async function attackCard(playercard, oppcard) {
+  //if (confirm(`Do you want to play the "${card.ugc_crd_name}" card?`)) {
+    let result = await requestAttackCard(playercard, oppcard);
+    if (result.successful) {
+      await getGameInfo();
+      await getBoardInfo();
+      await getDecksInfo();
+      alert("Card attacked!");
+    }
+  //}
 }
 
 async function closeScore() {
