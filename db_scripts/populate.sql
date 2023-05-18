0f5459b19f1673e44643320f4c5c02a4e9ae0769
MilkaII <fabiogaspar2603@gmail.com>
2023-05-18 14:13:40 +0100

    database update

--- db_scripts/populate.sql
+++ db_scripts/populate.sql
@@ -32,18 +32,26 @@ insert into card_state (crd_state) values ('In Board');
 insert into card_state (crd_state) values ('Killed');
 
 insert into card (crd_id, crd_cost, crd_damage, crd_health, crd_bonus, crd_name, crd_gang, crd_info, crd_hack_type_id, crd_type_id) values 
-   (1, 5, 5, 25, null, "Cipher","Neon Syndicate", null, null, 1),
-   (2, 2, 9, 6, null, "Armitager", "Neon Syndicate", null, null, 3),
-   (3, 2, 1, 12, null, "Gunnar","Neon Syndicate", null, null, 2),
-   (4, 2, 2, 1, null, "Rezin","Neon Syndicate", null, null, 2),
-   (5, 2, 0, 0, 3, "Maxine","Neon Syndicate", "Give +3 damage to a unit", 2, 4),
-   (6, 5, 5, 25, null, "Makko","Data Devils", null, null, 1),
-   (7, 2, 9, 6, null, "Erythrina", "Data Devils", null, null, 3),
-   (8, 2, 1, 12, null, "Vyrva","Data Devils", null, null, 2),
-   (9, 2, 2, 1, null, "Dredd Officer","Data Devils", null, null, 2),
-   (10, 2, 0, 0, 3, "Deckard","Data Devils", "Give +3 Health to a unit", 1, 4);
+   (1, 0, 0, 250, null, "Cipher","Neon Syndicate", null, null, 1), /*QTY = 1 */ /* CARD TYPE: LEADER */
+   (2, 9, 80, 90, null, "Armitager", "Neon Syndicate", null, null, 3), /*QTY = 1 */ /* CARD TYPE: HIGH RANKING MEMBER */
+   (3, 8, 65, 85, null, "Gunnar", "Neon Syndicate", null, null, 3), /*QTY = 2 */ /* CARD TYPE: HIGH RANKING MEMBER */
+   (4, 7, 50, 60, null, "Rezin", "Neon Syndicate", null, null, 3), /*QTY = 2 */ /* CARD TYPE: HIGH RANKING MEMBER */
+   (5, 6, 40, 50, null, "Maxine","Neon Syndicate", null, null, 2), /*QTY = 2 */ /* CARD TYPE: MEMBER */
+   (6, 4, 30, 45, null, "Geneve","Neon Syndicate", null, null, 2), /*QTY = 3 */ /* CARD TYPE: MEMBER */
+   (7, 3, 20, 30, null, "5","Neon Syndicate", null, null, 2), /*QTY = 3 */ /* CARD TYPE: MEMBER */
+   (8, 2, 0, 0, 3, "6","Neon Syndicate", "Give +3 damage to a unit", 2, 4), /*QTY = 3 */ /* CARD TYPE: HACKS */
+   (9, 2, 0, 0, 3, "7","Neon Syndicate", "Give +3 health to a unit", 1, 4), /*QTY = 3 */ /* CARD TYPE: HACKS */
+   (10, 0, 0, 250, null, "Makko","Data Devils", null, null, 1), /*QTY = 1 */ /* CARD TYPE: LEADER */
+   (11, 9, 80, 90, null, "Erythrina", "Data Devils", null, null, 3), /*QTY = 1 */ /* CARD TYPE: HIGH RANKING MEMBER */
+   (12, 8, 60, 80, null, "Vyrva", "Data Devils", null, null, 3), /*QTY = 2 */ /* CARD TYPE: HIGH RANKING MEMBER */
+   (13, 7, 55, 65, null, "Dredd Officer", "Data Devils", null, null, 3), /*QTY = 2 */ /* CARD TYPE: HIGH RANKING MEMBER */
+   (14, 6, 40, 50, null, "Deckard","Data Devils", null, null, 2), /*QTY = 2 */ /* CARD TYPE: MEMBER */
+   (15, 4, 30, 40, null, "Dex skill","Data Devils", null, null, 2), /*QTY = 3 */ /* CARD TYPE: MEMBER */
+   (16, 3, 25, 35, null, "5","Data Devils", null, null, 2), /*QTY = 3 */ /* CARD TYPE: MEMBER */
+   (17, 2, 0, 0, 3, "6","Data Devils", "Give +3 damage to a unit", 2, 4), /*QTY = 3 */ /* CARD TYPE: HACKS */
+   (18, 2, 0, 0, 3, "7","Data Devils", "Give +3 health to a unit", 1, 4); /*QTY = 3 */ /* CARD TYPE: HACKS */
    
-insert into deck (deck_id, deck_crd_id, deck_crd_qty) values (1, 1, 1), (1, 2, 2), (1, 3, 3), (1, 4, 2), (1, 5, 2), (2, 6, 1), (2, 7, 2), (2, 8, 3), (2, 9, 2), (2, 10, 2);
+insert into deck (deck_id, deck_crd_id, deck_crd_qty) values (1, 1, 1), (1, 2, 1), (1, 3, 2), (1, 4, 2), (1, 5, 2), (1, 6, 3), (1, 7, 3), (1, 8, 3), (1, 9, 3),(2, 10, 1), (2, 11, 1), (2, 12, 2), (2, 13, 2), (2, 14, 2), (1, 15, 3), (1, 16, 3), (1, 17, 3), (1, 18, 3);
 
 insert into game_board (gb_pos) values (1),(2),(3),(4),(5);
 insert into game_bench (gben_pos) values (1),(2),(3),(4),(5);
