8ce44b31e1db325a3f135455a6671361923c654c
Fábio Gaspar <fabiogaspar2603@gmail.com>
2025-11-04 10:41:46 +0000

    changes in index.js on line 46

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.use((err, req, res, next) => {
   res.status(500).send(err);
 })
 
-const port = parseInt(process.env.port || '8080');
+const port = parseInt(process.env.PORT || '8080', 10);
 app.listen(port,function() {
   console.log("Server running at http://localhost:"+port);
 });
